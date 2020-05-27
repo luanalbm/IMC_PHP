a98ed08692381c8d8b28e634dd2de2776935b7f6
LuanaBrito <luana.britomenezes@gmail.com>
2020-05-27 01:30:25 -0300

    Cálculo de IMC - Teste PHP

---
+++ servidor.php
@@ -0,0 +1,24 @@
+<?php
+// Receber textos do index
+    $nome = $_GET['nome_pessoa'];
+    $peso = $_GET['peso'];
+    $altura = $_GET['altura'];
+
+    $imc = $peso / ($altura*$altura);
+
+    if ($imc <= 1){
+        echo "Entrada invalida";
+    }elseif($imc <= 18.5){
+        echo "Meu nome é ".$nome.", meu IMC é ".$imc." e estou Abaixo do Peso";
+    }elseif($imc <= 24.99){
+        echo "Meu nome é ".$nome.", meu IMC é ".$imc." e estou com Peso Normal"; 
+    }elseif($imc <= 29.99){
+        echo "Meu nome é ".$nome.", meu IMC é ".$imc." e estou com Sobrepeso"; 
+    }elseif($imc <= 34.99){
+        echo "Meu nome é ".$nome.", meu IMC é ".$imc." e estou com Obesidade Grau 1"; 
+    }elseif($imc <=39.99){
+        echo "Meu nome é ".$nome.", meu IMC é ".$imc." e estou com Obesidade Grau 2"; 
+    }elseif($imc >= 40){
+        echo "Meu nome é ".$nome.", meu IMC é ".$imc." e estou com Obesidade Grau 3";
+    }
+?>
